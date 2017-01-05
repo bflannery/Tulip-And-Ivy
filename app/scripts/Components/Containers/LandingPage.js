1f14e5e6942cd76ae6ebcd37567904b94cbd04fa
Brian Flannery <bflannery66@gmail.com>
2017-01-05 07:57:47 -0600

    responsive landing page

--- app/scripts/Components/Containers/LandingPage.js
+++ app/scripts/Components/Containers/LandingPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import HeaderNav from '../HeaderNav';
 
+
 export default React.createClass({
   render() {
     return (
       <div className="main-container">
       <div className="landing-page-container">
-        <HeaderNav />
+        <img className="landing-logo" src='../../assets/images/tulipivylogo.jpg'/>
       </div>
+        <HeaderNav />
       </div>
     );
   }
