d8154d15d5371fcba1d9c13ba72955aa95ea9e12
Brian Flannery <bflannery66@gmail.com>
2017-01-07 19:42:44 -0600

    responsive about page

--- app/scripts/Components/Containers/AboutPage.js
+++ app/scripts/Components/Containers/AboutPage.js
@@ -7,7 +7,13 @@ export default React.createClass({
     return (
       <div className="about-page">
       <Header />
-      <div> "About" </div>
+      <div  className="about">
+      <div className="about-container">
+        <h3> Hi! Im Sophie! </h3>
+        <span> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <img className="avatar" src='../../assets/images/sophie.jpg'/>
+        </div>
       <Footer />
       </div>
     );
