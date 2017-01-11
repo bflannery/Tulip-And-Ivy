48b636a9217866515d3ae8f98f57e21e5ade9608
Brian Flannery <bflannery66@gmail.com>
2017-01-11 16:05:00 -0600

    updated gallery

--- app/scripts/Components/Containers/GalleryPage.js
+++ app/scripts/Components/Containers/GalleryPage.js
@@ -20,8 +20,8 @@ export default React.createClass({
 const FLOWERSET_1 = [
   {
     src: '../../assets/images/flowerset1-1.jpg',
-    width: 400,
-    height: 400,
+    width: 1802,
+    height: 2048,
     aspectRatio: 1,
     lightboxImage:{
     src: '../../assets/images/flowerset1-1.jpg',
@@ -34,54 +34,24 @@ const FLOWERSET_1 = [
     }
   },
   {
-    src: '../../assets/images/flowerset1-2.jpg',
-    width: 400,
-    height: 400,
+    src: '../../assets/images/flowerset1-8.jpg',
+    width: 1080,
+    height:1349,
     aspectRatio: 1,
     lightboxImage:{
-    src: '../../assets/images/flowerset1-2.jpg',
+    src: '../../assets/images/flowerset1-8.jpg',
     srcset: [
-      '../../assets/images/flowerset1-2.jpg 1024w',
-      '../../assets/images/flowerset1-2.jpg 800w',
-      '../../assets/images/flowerset1-2.jpg 500w',
-      '../../assets/images/flowerset1-2.jpg 320w',
-    ]
-    }
-  },
-  {
-    src: '../../assets/images/flowerset1-3.jpg',
-    width: 400,
-    height: 400,
-    aspectRatio: 1,
-    lightboxImage:{
-    src: '../../assets/images/flowerset1-3.jpg',
-    srcset: [
-      '../../assets/images/flowerset1-3.jpg 1024w',
-      '../../assets/images/flowerset1-3.jpg 800w',
-      '../../assets/images/flowerset1-3.jpg 500w',
-      '../../assets/images/flowerset1-3.jpg 320w',
-    ]
-    }
-  },
-  {
-    src: '../../assets/images/flowerset1-1.jpg',
-    width: 400,
-    height: 400,
-    aspectRatio: 1,
-    lightboxImage:{
-    src: '../../assets/images/flowerset1-1.jpg',
-    srcset: [
-      '../../assets/images/flowerset1-1.jpg 1024w',
-      '../../assets/images/flowerset1-1.jpg 800w',
-      '../../assets/images/flowerset1-1.jpg 500w',
-      '../../assets/images/flowerset1-1.jpg 320w',
+      '../../assets/images/flowerset1-8.jpg 1024w',
+      '../../assets/images/flowerset1-8.jpg 800w',
+      '../../assets/images/flowerset1-8.jpg 500w',
+      '../../assets/images/flowerset1-8.jpg 320w',
     ]
     }
   },
   {
     src: '../../assets/images/flowerset1-2.jpg',
-    width: 400,
-    height: 400,
+    width: 1543,
+    height: 2048,
     aspectRatio: 1,
     lightboxImage:{
     src: '../../assets/images/flowerset1-2.jpg',
@@ -94,9 +64,39 @@ const FLOWERSET_1 = [
     }
   },
   {
+    src: '../../assets/images/flowerset1-9.jpg',
+    width: 1080,
+    height:1349,
+    aspectRatio: 1,
+    lightboxImage:{
+    src: '../../assets/images/flowerset1-9.jpg',
+    srcset: [
+      '../../assets/images/flowerset1-9.jpg 1024w',
+      '../../assets/images/flowerset1-9.jpg 800w',
+      '../../assets/images/flowerset1-9.jpg 500w',
+      '../../assets/images/flowerset1-9.jpg 320w',
+    ]
+    }
+  },
+  {
+    src: '../../assets/images/flowerset1-6.jpg',
+    width: 1080,
+    height:1349,
+    aspectRatio: 1,
+    lightboxImage:{
+    src: '../../assets/images/flowerset1-6.jpg',
+    srcset: [
+      '../../assets/images/flowerset1-6.jpg 1024w',
+      '../../assets/images/flowerset1-6.jpg 800w',
+      '../../assets/images/flowerset1-6.jpg 500w',
+      '../../assets/images/flowerset1-6.jpg 320w',
+    ]
+    }
+  },
+  {
     src: '../../assets/images/flowerset1-3.jpg',
-    width: 400,
-    height: 400,
+    width: 1442,
+    height: 2048,
     aspectRatio: 1,
     lightboxImage:{
     src: '../../assets/images/flowerset1-3.jpg',
@@ -107,5 +107,5 @@ const FLOWERSET_1 = [
       '../../assets/images/flowerset1-3.jpg 320w',
     ]
     }
-  }
+  },
 ];
